db3ef2baadf5615ee70eb40465c1258b414c0d3f
Nguyen Ngoc Thien <kakangocthien109@gmail.com>
2025-12-10 23:30:26 +0700

    fix: secondary navigation visibility bug
    - Fixed secondary navigation bar not displaying due to CSS specificity issue
    - Bootstrap's .d-print-none utility was overriding display rules
    - Added final override in _security.scss (loaded last) with higher specificity
    - Ensures secondary-navigation.d-print-none.moove and children are visible
    - Bump version from 2025121001 to 2025121002

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_luuniensuki';
 
 // This is the version of the plugin.
-$plugin->version = 2025121001;
+$plugin->version = 2025121002;
 
 // This is the named version.
 $plugin->release = '0.0.1-alpha';
